36426d6ed6e470953b2b016b44671f38073c0c95
RadhikaAgrawal <radhika10agrawal@gmail.com>
2022-01-05 12:49:57 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -3,48 +3,64 @@ import { Modal } from 'react-bootstrap';
 
 function Signup(){
   console.log("Signed Up");
-}
+} 
 
-function checkLog(data = {}){
-    fetch('', {
-      method: 'POST', 
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
-    });
-}
-
-function checkLogin(){
+function checkLogin(Username, Password){
+  const requestData = {'Username':Username, 'Password': Password};
+  //console.log(requestData);
   fetch('http://localhost:3001',{
-    mode: 'no-cors'
+    method: 'POST',
+    headers :{
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(requestData)
   })
   .then(response => {
     if (!response.ok) {
-      console.log(response);
       throw new Error('Network response was not OK');
     }
-//    return response.blob();
+    //console.log(response);
+    return response.json();
   })
-  .then(data => console.log(data))
   .catch(error => {
     console.error('There has been a problem with your fetch operation:', error);
   }) 
 }
 
 class App extends Component{
-  state={
-    isModalOpen : false
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password:'',
+      isModalOpen : false
+    };
+
+    this.handleUserChange = this.handleUserChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleUserChange(event) {
+    this.setState({username: event.target.value});
+  }
+
+  handlePasswordChange(event) {
+    this.setState({password: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.username);
+    event.preventDefault();
+  }
 
   openModal = () => this.setState({ isModalOpen: true });
 
   render(){
      return (
-       <>
-      <button className="primary" onClick={this.openModal}>Login</button>
-      <button className="primary" onClick={Signup}>Signup</button>
+      <>
+      <button type='button' className="primary" onClick={this.openModal}>Login</button>
+      <button type='button' className="primary" onClick={Signup}>Signup</button>
       <Modal  show={this.state.isModalOpen}>
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
@@ -52,14 +68,15 @@ class App extends Component{
         <Modal.Body>
         <label>
           <p>Username</p>
-          <input type="text" name="Username" />
+          <input type="text" value={this.state.username} onChange={this.handleUserChange}/>
         </label>
         <label>
           <p>Password</p>
-          <input type="password" name="Password"/>
+          <input type="password" value={this.state.password} onChange={this.handlePasswordChange}/>
         </label>
         <div>
-          <button className="primary" onClick={checkLogin}>Submit</button>
+          <button type='submit' className="primary" onClick={checkLogin(this.state.username, this.state.password)}>
+              Submit</button>
         </div>
         </Modal.Body>
         <Modal.Footer>
